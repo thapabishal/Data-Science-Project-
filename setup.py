977fb01c0d9c7c9b08edf9bfd7740b15d951d14d
Bishal Khatri <thapabishal1245@gmail.com>
2025-11-30 09:08:14 +0545

    Project Structure

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
+# HYPEN_E_DOT = "-e ."
 
 def get_requirements(file_path: str) -> List[str]:
     """
@@ -11,10 +10,10 @@ def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("\n", "") for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        # if HYPEN_E_DOT in requirements:
+        #     requirements.remove(HYPEN_E_DOT)
 
         return requirements
 
